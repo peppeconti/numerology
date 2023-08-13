7d261b2aff8cc822e67b66d4aa83fa3750e882c6
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-08-13 16:07:29 +0200

    update

--- api/utils/results.js
+++ api/utils/results.js
@@ -17,28 +17,31 @@ const couples = {
       en: "4 is definitely compatible with 1, stable, reliable and responsible; however it would be even better if 4 had, in its numerological map, even more fun and adventurous numbers.",
     },
     5: {
-      it: "",
-      en: "",
+      it: "La compatibilità di questi due numeri è la migliore. A entrambi piace avere molta libertà in una relazione, e l'unica vera minaccia è quando cercano di imporre la loro volontà all'altro. I tratti della personalità di questa combinazione possono sia creare che distruggere, ma come il vento e il fuoco, dipendono l'uno dall'altro, si rispettano e camminano fianco a fianco per raggiungere i loro obiettivi e sogni, e nulla può resistere alla loro forza combinata.",
+      en: "The compatibility of these two numbers is the best. They both like to have a lot of freedom in a relationship, and the only real threat is when they try to impose their will on the other. The personality traits of this combination can both create and destroy, but like wind and fire, they depend on each other, respect each other and walk side by side to achieve their goals and dreams, and nothing can resist their combined force.",
     },
     6: {
-      it: "",
-      en: "",
+      it: "Tra il 6 e l'1 c'è il potenziale per creare relazioni durature, senza il rischio di vivere una relazione turbolenta come invece sperimentano molte altre relazioni. È importante che 1 e 6 capiscano come vedono le cose in modo diverso. Le loro priorità sono diverse e se possono essere consapevoli delle reciproche differenze e possono superare questo ostacolo, questo potrebbe essere un abbinamento di successo… La chiave è valorizzare le reciproche qualità e darsi il supporto di cui entrambi hanno bisogno.",
+      en: "Between 6 and 1 is the potential to create lasting relationships, without the risk of experiencing a turbulent relationship as many other relationships experience. It is important that 1 and 6 understand how they see things differently. Their priorities are different and if they can be aware of each other's differences and can overcome this obstacle, this could be a successful match… The key is to value each other's qualities and give each other the support they both need.",
     },
     7: {
-      it: "",
-      en: "",
+      it: "La compatibilità tra questi numeri è imprevedibile, infatti, alcune relazioni tra 1 e 7 fioriscono e altre no. L' 1 è il numero della volontà, il pensare fuori dagli schemi in modo aperto e non convenzionale, che contribuisce a una grande condivisione intellettuale con il 7. Due persone che si avventurano felicemente nel regno intellettuale e spirituale (conosciuto o sconosciuto) e grazie a ciò la relazione può essere molto promettente. Liberi pensatori, guidati da diverse prospettive ed energie diverse, per ragioni diverse, in qualche modo combaciano bene.",
+      en: "The compatibility between these numbers is unpredictable, in fact, some relationships between 1 and 7 flourish and some do not. The 1 is the number of will, thinking outside the box in an open and unconventional way, contributing to a great deal of intellectual sharing with the 7. Two people happily venturing into the intellectual and spiritual realm (known or unknown) and thanks to this the report can be very promising. Freethinkers, driven by different perspectives and different energies, for different reasons, somehow fit together well.",
     },
     8: {
-      it: "",
-      en: "",
+      it: "Gli appartenenti all'1 e all'8 sono forti, immobili e determinati. Sia che hanno ragione o torto, mantengono saldamente la loro posizione. Questa combinazione numerica crea relazioni appassionate e intense (di amore e odio). A causa della vostra natura irremovibile e a volte un pò egoista, possono sorgere dei conflitti. Entrambe dovete capire che il compromesso è la chiave del successo nella vita. Il successo di questo rapporto dipende dall'apertura e dalla reciproca disponibilità a scendere a compromessi e limitare le richieste. Questa combinazione è buona per le partnership commerciali e i matrimoni.",
+      en: "Members of 1 and 8 are strong, immobile and determined. Whether they are right or wrong, they stand their ground firmly. This number combination creates passionate and intense (love and hate) relationships. Due to your adamant and sometimes a little selfish nature, conflicts can arise. You both need to understand that compromise is the key to success in life. The success of this relationship depends on openness and mutual willingness to compromise and limit demands. This combination is good for business partnerships and weddings.",
     },
     9: {
-      it: "",
-      en: "",
+      it: "Il 9 e l'1 hanno la tendenza ad essere arroganti e in qualche modo egocentrici, questa relazione può essere molto difficile dal punto di vista romantico. Funziona solo se si mantiene una certa distanza (ognuno di voi deve vivere la propria vita). Ironia della sorte, nonostante le difficoltà, questa non è una cattiva combinazione per le relazioni di amicizia, relazioni genitore-figlio e relazioni d'affari, infatti di solito funzionano. 1 e 9 sono agli estremi opposti e per questo motivo  si completano e si bilanciano a vicenda. Insieme rappresentano molti talenti e qualità utili, assicurando una potente combinazione nella maggior parte delle relazioni (a parte il romanticismo).",
+      en: "The 9 and 1 have a tendency to be arrogant and somewhat self-centered, this relationship can be very difficult romantically. It only works if you keep a certain distance (each of you has to live your own life). Ironically, despite the difficulties, this isn't a bad combination for friendship relationships, parent-child relationships, and business relationships—in fact, they usually work out. 1 and 9 are at opposite extremes and for this reason they complement and balance each other. Together they represent many talents and useful qualities, ensuring a powerful combination in most relationships (romance aside).",
     },
   },
   2: {
-    2: "10d",
+    2: {
+        it: "Questa relazione è un grande incontro tra due anime con un sano rispetto per il potere dell'affetto e dell'emozione. L'unico avvertimento su questa combinazione è che, essendo due persone molto sensibili, devi ricordare quanto sei vulnerabile alle critiche, quindi potrebbe essere necessario affrontarle consapevolmente per evitare ferite verbali reciproche. Questo di solito non è un problema, poiché entrambi vi rispettate e avete la capacità di relazionarvi alle esperienze emotive dell'altro.",
+        en: "This relationship is a great meeting between two souls with a healthy respect for the power of affection and emotion. The only caveat to this combination is that, as two highly sensitive people, you need to remember how vulnerable you are to criticism, so you may need to deal with it consciously to avoid mutual verbal hurt. This is usually not a problem, as you both respect each other and have the ability to relate to each other's emotional experiences."
+    },
     3: "11d",
     4: "12d",
     5: "13d",
@@ -83,11 +86,20 @@ const couples = {
     9: "42d",
   },
   8: {
-    8: "43d",
-    9: "44d",
+    8: {
+        it: "Tra due 8 risulta esserci una buona compatibilità, non solo in amore, ma anche negli affari. Avere due 8 in una relazione mette ognuno di voi nella posizione di sostenervi a vicenda. Le insidie sorgono quando ti ritrovi a competere l'uno contro l'altro o davanti a questioni di soldi. Se riesci a combinare la tua natura competitiva con altre forze e seguire il tuo cuore e il tuo intuito per perseguire il successo, ci sono buone probabilità che la coppia abbia una relazione lunga e felice.",
+        en: "Between two 8s there is good compatibility, not only in love, but also in business. Having two 8s in a relationship puts each of you in a position to support each other. Pitfalls arise when you find yourself competing against each other or when faced with money issues. If you can combine your competitive nature with other strengths and follow your heart and intuition to pursue success, there's a good chance the couple will have a long and happy relationship."
+    },
+    9: {
+        it: "Tra 8 e 9 non è una relazione considerata molto compatibile. I vostri obiettivi sono molto diversi e vi potreste ritrovare spesso a discutere. Anche se c'è una forte attrazione che può costituire la base di una relazione interessante, l'unico modo in cui questa coppia può sopravvivere è riconoscere e rispettare le grandi differenze reciproche.",
+        en: "Between 8 and 9 is not a relationship considered very compatible. Your goals are very different and you may often find yourself arguing. While there is a strong attraction that can form the basis of an interesting relationship, the only way this couple can survive is to acknowledge and respect each other's vast differences."
+    },
   },
   9: {
-    9: "45d",
+    9: {
+        it: "La compatibilità dei due 9 è eccezionale e porta molta speranza per una relazione stimolante e felice. Vi assomigliate e la vostra natura altruista pone pochi problemi in questa relazione. Potrebbero esserci delle insidie, ma c'è una buona possibilità che questo insieme di numeri formi un legame indissolubile… che non si indebolirà nel tempo.",
+        en: "The compatibility of the two 9s is exceptional and carries a lot of hope for a challenging and happy relationship. You are alike and your selfless nature poses few problems in this relationship. There may be pitfalls, but there's a good chance this set of numbers will form an unbreakable bond… one that won't weaken over time."
+    },
   },
 };
 
