cce1f997c2ac35efcac8e4e7f6532a88236f2906
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-08-13 22:45:34 +0200

    update

--- api/routes/affinity-route.js
+++ api/routes/affinity-route.js
@@ -14,7 +14,7 @@ router.post(
   query("him").not().isEmpty().withMessage("The field couldn't be empty").isDate({
     format: 'DD-MM-YYYY'
   }).withMessage("Please insert a valid date: DD-MM-YYYY"),
-  query("lan", "The language value should be either 'it' oder 'en'").isIn(["it", "en"]).optional(),
+  query("lan", "The language value should be either 'it' or 'en'").isIn(["it", "en"]).optional(),
   affinityControllers.calculateAffinity
 );
 
