290f14d5369fa57e6fd0215359a7bc340e246bc7
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-08-21 22:43:34 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const path = require('path');
-const favicon = require('express-favicon');
+const path = require("path");
+const favicon = require("express-favicon");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 const HttpError = require("./models/http-error");
 
 // BODYPARSER
@@ -14,8 +14,8 @@ const affinityRoutes = require("./api/routes/affinity-route");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(__dirname + '/assets/icons.png'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(__dirname + "/assets/icons.png"));
 
 app.use(cors());
 
@@ -24,25 +24,29 @@ app.use("/api/affinity", affinityRoutes);
 
 // ROUTE NOT FOUND
 app.use((req, res, next) => {
-    const error = new HttpError("Could not find this route", ["Could not find this route"], 404);
-    throw error;
-  });
+  const error = new HttpError(
+    "Could not find this route",
+    ["Could not find this route"],
+    404
+  );
+  throw error;
+});
 // GENERAL ERROR HANDLING
-app.use('/api', (error, req, res, next) => {
-    res.status(error.code || 500);
-    res.json({
-        status: false,
-        error: {
-            message: error.message,
-            messages: error.messages,
-            code: error.code || 500
-        }
-    });
+app.use("/api", (error, req, res, next) => {
+  res.status(error.code || 500);
+  res.json({
+    status: false,
+    error: {
+      message: error.message,
+      messages: error.messages,
+      code: error.code || 500,
+    },
+  });
 });
 
-app.use('/', (error, req, res, next) => {
-    res.status(error.code || 500);
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+app.use("/", (error, req, res, next) => {
+  res.status(error.code || 500);
+  res.sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 module.exports = app;
