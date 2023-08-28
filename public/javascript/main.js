c0cefb9cff04f29b9a68bcf2bf9ebf8538962334
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-08-28 22:27:12 +0200

    update

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -10,34 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const affinity__values = {};
 
   [her, him, lan].forEach((input) => {
-    input.addEventListener(
-      "input",
-      (e) => {
-        if (e.target.value.length) affinity__values[input.name] = e.target.value;
-        if (!e.target.value.length) delete affinity__values[input.name];
-        console.log(affinity__values);
-      }
-    );
+    input.addEventListener("input", (e) => {
+      if (e.target.value.length) affinity__values[input.name] = e.target.value;
+      if (!e.target.value.length) delete affinity__values[input.name];
+      console.log(affinity__values);
+    });
   });
-
-  /*let herValue = "";
-  let himValue = "";
-  let lanValue = "";
-
-  her.addEventListener("input", (e) => {
-    herValue = e.target.value;
-    console.log(herValue);
-  });
-
-  him.addEventListener("input", (e) => {
-    himValue = e.target.value;
-    console.log(himValue);
-  });
-
-  lan.addEventListener("input", (e) => {
-    lanValue = e.target.value;
-    console.log(lanValue);
-  });*/
 
   // SCROLL
   const links = document.querySelectorAll(".nav__bar li a");
@@ -60,12 +38,21 @@ document.addEventListener("DOMContentLoaded", () => {
   // FETCH
 
   async function submit(e) {
+
     e.preventDefault();
-    console.log(window.location.origin);
-    const uri = `${window.location.origin}/api/affinity?her=${herValue}&him=${himValue}`;
-    const response = await fetch(uri);
+
+    let values =[];
+  
+    for (const property in affinity__values) {
+      let param = `${property}=${affinity__values[property]}`;
+      values.push(param);
+    }
+    
+    const uri = `${window.location.origin}/api/affinity?${values.join('&')}`;
+    console.log(uri);
+    /*const response = await fetch(uri);
     const affinity = await response.json();
 
-    affinity__response.innerHTML = `<pre>${JSON.stringify(affinity)}</pre>`;
+    affinity__response.innerHTML = `<pre>${JSON.stringify(affinity)}</pre>`;*/
   }
 });
