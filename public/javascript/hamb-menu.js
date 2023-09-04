d7d073d40ee289a20978713900b82cbafa8f22f4
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-09-04 14:44:07 +0200

    update

--- public/javascript/hamb-menu.js
+++ public/javascript/hamb-menu.js
@@ -2,6 +2,7 @@ const hamb = document.querySelector(".hamb__menu");
 const nav = document.querySelector(".nav__bar");
 
 function showMenu() {
+    hamb.children[0].classList.toggle('fa-times');
     nav.classList.toggle('visible');
 }
 
