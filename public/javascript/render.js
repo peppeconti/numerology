a034ca919733ffa7a9a5079d22f41c946e8bf31e
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-09-05 09:38:59 +0200

    update

--- public/javascript/render.js
+++ public/javascript/render.js
@@ -4,8 +4,8 @@ const preTemplate = (result, state = null) => {
   if (!state) return `<h3>Request:</h3><pre><a href="${result}" target="_blank">${result}</a></pre>`;
   if (state) return `<h3>Response: ${
       state==="true"
-        ? "<span style='color:rgb(182, 255, 182);'>V</span>"
-        : "<span style='color:rgb(255, 168, 168);'>X</span>"
+        ? "<span style='color: rgb(73, 255, 73);' class='fas fa-check-circle'></span>"
+        : "<span style='color: rgb(255, 87, 87);' class='fas fa-times-circle'></span>"
     }</h3><pre ${state==="true" ? "style='background-color:rgb(182, 255, 182);'" : "style='background-color:rgb(255, 168, 168);'"}>${result}</pre>`;
 };
 
