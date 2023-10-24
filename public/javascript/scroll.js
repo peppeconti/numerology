7734d8bf3c709104d5c72cfcccf1278e123240e1
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-10-24 12:52:11 +0200

    update

--- public/javascript/scroll.js
+++ public/javascript/scroll.js
@@ -5,10 +5,28 @@ const clickHandler = (e) => {
   const href = e.target.getAttribute("href");
   const offsetTop = document.querySelector(href).offsetTop;
 
+  //console.log("click: "+ String(offsetTop));
+
   scroll({
     top: offsetTop - 100,
     behavior: "smooth",
   });
 };
+
+window.addEventListener('scroll', (e) => {
+ 
+  links.forEach(link => {
+    //console.log("scroll: "+ String(window.scrollY));
+    const href = link.getAttribute("href");
+    //console.log(document.querySelector(href).offsetTop);
+    //console.log(document.querySelector(href));
+    //console.log("scroll: "+ String(window.scrollY+100));
+    //console.log(window.scrollY >= document.querySelector(href).offsetTop-100);
+    if (window.scrollY+100 >= document.querySelector(href).offsetTop) {
+      console.log(href);
+    }
+  })
+
+})
 
 export { links, clickHandler };
