c6a904ca09b34773641e6c0ba6318dfc0f472c7a
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-10-25 19:40:34 +0200

    update

--- public/javascript/hamb-menu.js
+++ public/javascript/hamb-menu.js
@@ -1,19 +1,25 @@
 const hamb = document.querySelector(".hamb__menu");
 const nav = document.querySelector(".nav__bar");
 
+const fadeInKeyframes = new KeyframeEffect(
+  nav,
+  [{ right: "-100%" }, { right: 0 }],
+  { duration: 500, fill: "forwards" }
+);
+
+const fadeOutKeyframes = new KeyframeEffect(
+  nav,
+  [{ right: 0 }, { right: "-100%" }],
+  { duration: 500, fill: "forwards" }
+);
+
 function showMenu() {
-    console.log(nav.classList.contains('visible'));
-    if (nav.classList.contains('visible')) {
-        nav.classList.add('back');
-        setTimeout(() => {
-            hamb.children[0].classList.toggle('fa-times');
-            nav.classList.toggle('visible');
-        }, 500)
-    } else if (!nav.classList.contains('visible')) {
-        hamb.children[0].classList.toggle('fa-times');
-        nav.classList.remove('back');
-        nav.classList.toggle('visible');
-    }
+  let animateNav;
+  hamb.children[0].classList.contains("fa-times")
+    ? (animateNav = new Animation(fadeOutKeyframes, document.timeline))
+    : (animateNav = new Animation(fadeInKeyframes, document.timeline));
+  hamb.children[0].classList.toggle("fa-times");
+  animateNav.play();
 }
 
 export { hamb, showMenu };
