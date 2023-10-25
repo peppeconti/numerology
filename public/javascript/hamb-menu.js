6c86bf747f664f1ef75b1f9d8e6b308079c8e619
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-10-25 16:48:42 +0200

    update

--- public/javascript/hamb-menu.js
+++ public/javascript/hamb-menu.js
@@ -2,8 +2,18 @@ const hamb = document.querySelector(".hamb__menu");
 const nav = document.querySelector(".nav__bar");
 
 function showMenu() {
-    hamb.children[0].classList.toggle('fa-times');
-    nav.classList.toggle('visible');
+    console.log(nav.classList.contains('visible'));
+    if (nav.classList.contains('visible')) {
+        nav.classList.add('back');
+        setTimeout(() => {
+            hamb.children[0].classList.toggle('fa-times');
+            nav.classList.toggle('visible');
+        }, 500)
+    } else if (!nav.classList.contains('visible')) {
+        hamb.children[0].classList.toggle('fa-times');
+        nav.classList.remove('back');
+        nav.classList.toggle('visible');
+    }
 }
 
 export { hamb, showMenu };
